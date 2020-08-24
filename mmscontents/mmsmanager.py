ef75b29df9a9a0eeabca21cbad24f3d771973ad2
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2020-08-23 22:41:29 -0700

    saving work - display mms orgs and projects as directories

--- mmscontents/mmsmanager.py
+++ mmscontents/mmsmanager.py
@@ -8,8 +8,10 @@ from notebook.services.contents.filecheckpoints import GenericFileCheckpoints
 
 from tornado.web import HTTPError
 
-from mmscontents.service import get_notebooks, get_notebook, save_notebook, get_mms_token
+import mmscontents.service as service
+from mmscontents.checkpoints import NoOpCheckpoints
 from traitlets import Unicode
+from traitlets import default
 
 DUMMY_CREATED_DATE = datetime.datetime.fromtimestamp(86400)
 NBFORMAT_VERSION = 4
@@ -17,105 +19,103 @@ NBFORMAT_VERSION = 4
 
 class MMSContentsManager(ContentsManager):
 
-    mms_url = Unicode("https://mms.openmbee.org", help="MMS endpoint").tag(config=True, env="JPYNB_MMS_URL")
-    mms_project = Unicode("a", help="MMS project id").tag(config=True, env="JPYNB_MMS_PROJECT_ID")
+    mms_url = Unicode("http://localhost:8080", help="MMS endpoint").tag(config=True, env="JPYNB_MMS_URL")
     mms_username = Unicode("dummy", help="MMS username").tag(config=True, env="JPNYB_MMS_USERNAME")
     mms_password = Unicode("dummy", help="MMS password").tag(config=True, env="JPNYB_MMS_PASSWORD")
     _mms_token = ""
+    _mms_projects = {}
+    _mms_orgs = {}
     
     def __init__(self, *args, **kwargs):
         super(MMSContentsManager, self).__init__(*args, **kwargs)
-        self._mms_token = get_mms_token(self.mms_url, self.mms_username, self.mms_password)
+        self._mms_token = service.get_mms_token(self.mms_url, self.mms_username, self.mms_password)
+        self._mms_orgs = service.get_orgs_with_jupyter_projects(self.mms_url, self._mms_token)
+        self._mms_projects = service.get_jupyter_projects(self.mms_url, self._mms_token)
 
     def dir_exists(self, path):
         print('?dir exists ' + path)
-        if path == '' or path == '/':
+        realpath = get_normalized_path(path)
+        if realpath == '':
             return True
-        return False
+        paths = realpath.split('/')
+        if (len(paths) > 2) or (len(paths) < 2 and paths[0] not in self._mms_orgs) or \
+                (len(paths) == 2 and paths[1] not in self._mms_projects):
+            return False
+        return True
 
     def is_hidden(self, path):
         return False
 
     def file_exists(self, path=''):
         print('?file exists ' + path)
-        if path == '' or path == '/':
+        realpath = get_normalized_path(path)
+        if len(realpath.split('/')) <= 2:
             return False
-        id = get_id_from_path(path)
-        return id in get_notebooks(self.mms_url, self.mms_project, self._mms_token)
+        ids = get_ids_from_path(realpath)
+        return ids[2] in service.get_notebooks(self.mms_url, ids[1], self._mms_token)
 
     def get(self, path, content=True, type=None, format=None):
         print('?get ' + path + ' ' + str(content) + ' ' + str(type) + ' ' + str(format))
+        realpath = get_normalized_path(path)
         if type is None:
-            type = self.guess_type(path)
-        try:
-            func = {
-                'directory': self._get_directory,
-                'notebook': self._get_notebook,
-                'file': self._get_file,
-            }[type]
-        except KeyError:
-            raise ValueError("Unknown type passed: '{}'".format(type))
-        return func(path=path, content=content, format=format)
+            type = self._guess_type(realpath)
+        func = {
+                'directory': self._directory_model_from_path,
+                'notebook': self._notebook_model_from_path,
+                'file': self._file_model_from_path,
+        }[type]
+        res = func(path=realpath, content=content, format=format)
+        return res
 
-    def _get_directory(self, path, content=True, format=None):
-        return self._directory_model_from_path(path, content=content)
-
-    def _get_notebook(self, path, content=True, format=None):
-        return self._notebook_model_from_path(path, content=content, format=format)
-
-    def _get_file(self, path, content=True, format=None):
-        return self._file_model_from_path(path, content=content, format=format)
-
-    def _directory_model_from_path(self, path, content=False):
+    def _directory_model_from_path(self, path, content=False, format=None):
         print('?dir model from path ' + path + ' ' + str(content))
-        model = base_directory_model(path)
         if content:
             if not self.dir_exists(path):
-                self.no_such_entity(path)
-        if path == '' or path == '/':
-            content = []
-            for id, n in get_notebooks(self.mms_url, self.mms_project, self._mms_token).items():
+                self._no_such_entity(path)
+        model = None
+        ids = get_ids_from_path(path)
+        if path == '' or path == '/': #orgs
+            contents = []
+            for id, org in self._mms_orgs.items():
+                contents.append(directory_model(org.name, '/' + id, org, None))
+            model = directory_model('', '/', None, contents)
+        elif len(ids) == 1: #projects under an org
+            contents = []
+            for id, proj in self._mms_projects.items():
+                if proj.org_id == ids[0]:
+                    contents.append(directory_model(proj.name, '/' + ids[0] + '/' + id, proj, None))
+            org = self._mms_orgs[ids[0]]
+            model = directory_model(org.name, '/' + ids[0], org, contents)
+        elif len(ids) == 2: #notebooks under a project
+            contents = []
+            for id, n in service.get_notebooks(self.mms_url, ids[1], self._mms_token).items():
                 name = id + '.ipynb'
                 if 'mms' in n['metadata'] and 'name' in n['metadata']['mms']:
                     name = n['metadata']['mms']['name']
-                nmodel = base_model(name)
-                nmodel.update(
-                    name=name,
-                    type="notebook",
-                    format="json",
-                    last_modified=string_to_date(n['_modified']),
-                    created=string_to_date(n['_created']),
-                    path=id + '.ipynb'
-                )
-                content.append(nmodel)
-            model.update(content=content)
+                contents.append(notebook_model(name, '/' + ids[0] + '/' + ids[1] + '/' + id + '.ipynb', n, None))
+            proj = self._mms_projects[ids[1]]
+            model = directory_model(proj.name, '/' + ids[0] + '/' + ids[1], proj, contents)
         return model
 
     def _notebook_model_from_path(self, path, content=False, format=None):
         print('?notebook from path ' + path + ' ' + str(content))
         if not self.file_exists(path):
-            self.no_such_entity(path)
+            self._no_such_entity(path)
 
-        id = get_id_from_path(path)
-        notebook = get_notebook(self.mms_url, self.mms_project, id, self._mms_token)
-        name = id + '.ipynb'
+        ids = get_ids_from_path(path)
+        notebook = service.get_notebooks(self.mms_url, ids[1], self._mms_token)[ids[2]] #worksaround for get_notebook not working
+        name = ids[2] + '.ipynb'
         if 'mms' in notebook['metadata'] and 'name' in notebook['metadata']['mms']:
             name = notebook['metadata']['mms']['name']
-        model = base_model(path)
-        model.update(
-            last_modified=string_to_date(notebook['_modified']),
-            created=string_to_date(notebook['_created']),
-            type='notebook',
-            name=name
-        )
+        model = notebook_model(name, '/' + ids[0] + '/' + ids[1] + '/' + ids[2] + '.ipynb', notebook, None)
         if content:    
             #file_content = json.dumps(get_notebooks(mms_url, mms_project)[id])
             #nb_content = nb_format.reads(file_content, as_version=nbformat.NO_CONVERT)
             #self.mark_trusted_cells(nb_content, path)
             nb_content = nbformat.from_dict(move_id_to_metadata(notebook))
             model.update(
-                content=nb_content,
-                format='json'
+                content = nb_content,
+                format = 'json'
             )
             #self.validate_notebook_model(model)
         return model
@@ -125,26 +125,35 @@ class MMSContentsManager(ContentsManager):
         model = self._notebook_model_from_path(path, content, format)
         model['type'] = 'file'
         if content:
-            model["mimetype"] = "application/json"
+            model["mimetype"] = "application/x-ipynb+json"
         return model
 
     def save(self, model, path): 
         # for new cells it'll keep adding new ids and cell elements since ids doesn't go back to frontend
         # may need frontend extensions to add these ids as they're created
         print('?save ' + path + ' ' + str(model))
-        notebook = add_mms_id(model['content'])
+        realpath = get_normalized_path(path)
+        ids = get_ids_from_path(realpath)
+        if len(ids) <= 2:
+            self._do_error("cannot create here", 400)
+        if model['type'] != 'notebook':
+            self._do_error("cannot create non notebooks", 400)
+        notebook = add_mms_id(model['content'], ids[2])
         if 'name' not in notebook['metadata']['mms']:
             notebook['metadata']['mms']['name'] = 'Untitled.ipynb'
         print('notebook_to_save: ' + str(notebook))
-        notebook = save_notebook(self.mms_url, self.mms_project, notebook, self._mms_token)
-        ret = base_model(path)
-        ret.update(
-            path=notebook['id'] + '.ipynb',
-            last_modified=string_to_date(notebook['_modified']),
-            created=string_to_date(notebook['_created']),
-            type='notebook',
-            name=notebook['metadata']['mms']['name']
-        )
+        notebook = service.save_notebook(self.mms_url, ids[1], notebook, self._mms_token)
+        ret = {
+            "name": notebook['metadata']['mms']['name'],
+            "path": '/' + ids[0] + '/' + ids[1] + '/' + notebook['id'] + '.ipynb',
+            "writable": True,
+            "last_modified": string_to_date(notebook['_modified']),
+            "created": string_to_date(notebook['_created']),
+            "content": None,
+            "format": None,
+            "mimetype": None,
+            'type': 'notebook'
+        }
         return ret
         #return self.get(path, type='notebook', content=False)
 
@@ -152,75 +161,66 @@ class MMSContentsManager(ContentsManager):
         pass
 
     def rename_file(self, old_path, new_path):
-        #jupyter is treating name as the path...so this won't work
+        # the passed in path is using the name and not the actual path with id
         print("?rename " + old_path + " " + new_path)
-        id = get_id_from_path(old_path)
-        notebook = get_notebook(self.mms_url, self.mms_project, id, self._mms_token)
-        new_name = new_path.rsplit('/', 1)[-1]
+        real_old_path = get_normalized_path(old_path)
+        real_new_path = get_normalized_path(new_path)
+        old_ids = get_ids_from_path(real_old_path)
+        new_ids = get_ids_from_path(real_new_path)
+        if len(old_ids) != 3 or len(new_ids) != 3:
+            self._do_error('cannot rename non notebooks', 400)
+        old_name = old_ids[2] + '.ipynb'
+        new_name = new_ids[2] + '.ipynb'
+
+        notebooks = service.get_notebooks(self.mms_url, old_ids[1], self._mms_token)
+        #find notebook with the old name....
+        notebook = None
+        for id, n in notebooks.items():
+            if 'mms' in n['metadata'] and 'name' in n['metadata']['mms'] and n['metadata']['mms']['name'] == old_name:
+                notebook = n
+                break
+
         if 'metadata' not in notebook:
             notebook['metadata'] = {}
         if 'mms' not in notebook['metadata']:
-            notebook['metadata']['mms'] = {'id': notebook['id']}
+            notebook['metadata']['mms'] = {}
         notebook['metadata']['mms']['name'] = new_name
-        to_save = {'id': notebook['id'], 'metadata': notebook['metadata']}
-        notebook = save_notebook(self.mms_url, self.mms_project, to_save, self._mms_token)
-        ret = base_model(old_path)
-        ret.update(
-            name=new_name,
-            last_modified=string_to_date(notebook['_modified']),
-            created=string_to_date(notebook['_created']),
-            type='notebook'
-        )
-        return ret
+        notebook['metadata']['mms']['id'] = notebook['id']
 
-    def guess_type(self, path):
+        to_save = {'id': notebook['id'], 'metadata': notebook['metadata']}
+        notebook = service.save_element(self.mms_url, old_ids[1], to_save, self._mms_token)
+        ret = notebook_model(new_name, '/' + old_ids[0] + '/' + old_ids[1] + '/' + notebook['id'] + '.ipynb', notebook, None)
+        #return ret
+
+    def _guess_type(self, path):
         print('?guess type ' + path)
-        if path == '' or path == '/':
+        if len(path.split('/')) <= 2:
             return 'directory'
         return 'notebook'
 
-    def do_error(self, msg, code=500):
+    def _do_error(self, msg, code=500):
         raise HTTPError(code, msg)
 
-    def no_such_entity(self, path):
-        self.do_error("No such entity: [{path}]".format(path=path), 404)
+    def _no_such_entity(self, path):
+        self._do_error("No such entity: [{path}]".format(path=path), 404)
 
-    def _checkpoints_class_default(self):
-        return GenericFileCheckpoints
-
-def base_model(path):
-    return {
-        "name": path.rsplit('/', 1)[-1],
-        "path": path,
-        "writable": True,
-        "last_modified": None,
-        "created": None,
-        "content": None,
-        "format": None,
-        "mimetype": None,
-    }
-
-def base_directory_model(path):
-    model = base_model(path)
-    model.update(
-        type="directory",
-        last_modified=DUMMY_CREATED_DATE,
-        created=DUMMY_CREATED_DATE,
-        format="json",
-        content=[])
-    return model
+    @default('checkpoints_class')
+    def _default_checkpoints_class(self):
+        return GenericFileCheckpoints #NoOpCheckpoints
 
 def move_id_to_metadata(notebook):
     if 'mms' not in notebook['metadata']:
         notebook['metadata']['mms'] = {}
-    notebook['metadata']['mms']['id']: notebook['id']
+    notebook['metadata']['mms']['id'] = notebook['id']
     for i in notebook['cells']:
         i['metadata']['mms'] = {'id': i['id']}
     return notebook
 
-def add_mms_id(notebook):
+def add_mms_id(notebook, namepath=None):
     if 'mms' not in notebook['metadata']:
         notebook['metadata']['mms'] = {'id': str(uuid.uuid4())}
+        if namepath is not None:
+            notebook['metadata']['mms']['name'] = namepath + '.ipynb'
     notebook['id'] = notebook['metadata']['mms']['id']
     for i in notebook['cells']:
         if 'mms' not in i['metadata']:
@@ -228,9 +228,59 @@ def add_mms_id(notebook):
         i['id'] = i['metadata']['mms']['id']
     return notebook
 
-def get_id_from_path(path):
-    return path.rsplit('/', 1)[-1].split('.')[0]
+def get_ids_from_path(path):
+    paths = path.split('/')
+    if len(paths) > 2:
+        paths[2] = paths[2].rsplit('.', 1)[0]
+    return paths
 
 def string_to_date(s):
+    if s is None:
+        return DUMMY_CREATED_DATE
     s = s[:-5] + '000' + s[-5:]
     return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+def get_normalized_path(path):
+    return path.strip('/')
+
+def directory_model(name, path, ob, contents):
+    model = {
+        "name": name,
+        "path": path,
+        "writable": True,
+        "last_modified": DUMMY_CREATED_DATE,
+        "created": DUMMY_CREATED_DATE,
+        "content": contents,
+        "format": 'json',
+        "mimetype": None,
+        'type': 'directory'
+    }
+    if ob is not None:
+        model.update(
+            last_modified = string_to_date(ob.modified),
+            created = string_to_date(ob.created)
+        )
+    if contents is None:
+        model['format'] = None
+    return model
+
+def notebook_model(name, path, ob, contents):
+    model = {
+        "name": name,
+        "path": path,
+        "writable": True,
+        "last_modified": DUMMY_CREATED_DATE,
+        "created": DUMMY_CREATED_DATE,
+        "content": contents,
+        "format": 'json',
+        "mimetype": None,
+        'type': 'notebook'
+    }
+    if ob is not None:
+        model.update(
+            last_modified = string_to_date(ob['_modified']),
+            created = string_to_date(ob['_created'])
+        )
+    if contents is None:
+        model['format'] = None
+    return model
--- mmscontents/service.py
+++ mmscontents/service.py
@@ -1,11 +1,46 @@
 import mms_python_client as mms_client
 from mms_python_client.rest import ApiException
 
+def get_orgs_with_jupyter_projects(url, token):
+    configuration = mms_client.Configuration()
+    configuration.host = url
+    configuration.access_token = token
+    api = mms_client.OrgsApi(mms_client.ApiClient(configuration))
+    try:
+        res = {}
+        orgs = api.get_all_orgs().orgs
+        orgsDict = {}
+        for o in orgs:
+            orgsDict[o.id] = o
+        projectsMap = get_jupyter_projects(url, token)
+        for pid in projectsMap:
+            orgId = projectsMap[pid].org_id
+            if orgId in orgsDict:
+                res[orgId] = orgsDict[orgId]
+        return res
+    except ApiException as e:
+        print("Exception when calling OrgsApi->get_all_orgs: %s\n" % e)
+
+def get_jupyter_projects(url, token):
+    configuration = mms_client.Configuration()
+    configuration.host = url
+    configuration.access_token = token
+    api = mms_client.ProjectsApi(mms_client.ApiClient(configuration))
+    try:
+        res = api.get_all_projects()
+        ret = {}
+        for p in res.projects:
+            if p.schema == 'jupyter':
+                ret[p.id] = p
+        return ret
+    except ApiException as e:
+        print("Exception when calling ProjectsApi->get_all_projects: %s\n" % e)
+
 def get_notebooks(url, project, token):
     configuration = mms_client.Configuration()
     configuration.host = url
     configuration.access_token = token
-    api_instance = mms_client.NotebooksControllerApi(mms_client.ApiClient(configuration))
+    api_instance = mms_client.NotebooksApi(mms_client.ApiClient(configuration))
     try:
         api_response = api_instance.get_all_notebooks(project, 'master')
         ret = {}
@@ -13,40 +48,50 @@ def get_notebooks(url, project, token):
             ret[i['id']] = i
         return ret
     except ApiException as e:
-        print("Exception when calling NotebooksControllerApi->get_all_notebooks: %s\n" % e)
+        print("Exception when calling NotebooksApi->get_all_notebooks: %s\n" % e)
 
 def get_notebook(url, project, id, token):
     configuration = mms_client.Configuration()
     configuration.host = url
     configuration.access_token = token
-    api_instance = mms_client.NotebooksControllerApi(mms_client.ApiClient(configuration))
+    api_instance = mms_client.NotebooksApi(mms_client.ApiClient(configuration))
     try:
         api_response = api_instance.get_notebook(project, 'master', id)
         return api_response.notebooks[0]
     except ApiException as e:
-        print("Exception when calling NotebooksControllerApi->get_notebook: %s\n" % e)
+        print("Exception when calling NotebooksApi->get_notebook: %s\n" % e)
+
+def save_element(url, project, element, token):
+    configuration = mms_client.Configuration()
+    configuration.host = url
+    configuration.access_token = token
+    api_instance = mms_client.ElementsApi(mms_client.ApiClient(configuration))
+    try:
+        r = mms_client.ElementsRequest(elements=[element])
+        api_response = api_instance.create_or_update_elements(project, 'master', elements_request=r)
+        return api_response.elements[0]
+    except ApiException as e:
+        print("Exception when calling NotebooksApi->create_or_update_notebooks: %s\n" % e)
 
 def save_notebook(url, project, notebook, token):
     configuration = mms_client.Configuration()
     configuration.host = url
     configuration.access_token = token
-    api_instance = mms_client.NotebooksControllerApi(mms_client.ApiClient(configuration))
+    api_instance = mms_client.NotebooksApi(mms_client.ApiClient(configuration))
     try:
-        r = mms_client.NotebooksRequest()
-        r.notebooks = [notebook]
+        r = mms_client.NotebooksRequest(notebooks=[notebook])
         api_response = api_instance.create_or_update_notebooks(project, 'master', notebooks_request=r)
         return api_response.notebooks[0]
     except ApiException as e:
-        print("Exception when calling NotebooksControllerApi->create_or_update_notebooks: %s\n" % e)
+        print("Exception when calling NotebooksApi->create_or_update_notebooks: %s\n" % e)
 
 def get_mms_token(url, username, password):
+    print('?get_mms_token ' + url + ' ' + username)
     configuration = mms_client.Configuration()
     configuration.host = url
-    jwt_authentication_request = mms_client.JwtAuthenticationRequest()
-    jwt_authentication_request.username = username
-    jwt_authentication_request.password = password
+    jwt_authentication_request = mms_client.JwtAuthenticationRequest(username, password)
     try:
-        api_instance = mms_client.AuthenticationControllerApi(mms_client.ApiClient(configuration))
+        api_instance = mms_client.AuthApi(mms_client.ApiClient(configuration))
         api_response = api_instance.create_authentication_token(jwt_authentication_request=jwt_authentication_request)
         return api_response.token
     except ApiException as e:
